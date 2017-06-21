9572ebb2bb7855fe42d8f94491aa11f8cc190ed5
lerissonf <lff3@cin.ufpe.br>
2017-06-21 18:46:47 -0300

    atualização/commit final

--- src/arquivoordena/ProgramaPrincipal.java
+++ src/arquivoordena/ProgramaPrincipal.java
@@ -20,78 +20,92 @@ public class ProgramaPrincipal {
        
         @SuppressWarnings("resource")
 		Scanner ler = new Scanner(System.in);
-		System.out.printf("Informe o caminho de saida do arquivo texto:\n");
+        System.out.printf("Informe o caminho de saida do arquivo texto:\n");
 		String caminho = ler.nextLine();
-        boolean boleano = true;
-        while(boleano) 
-        {
-        	
-        	System.out.print( "informe os valores a serem ordenados um por vez ou 909909 para parar de adicionar: " );
-            String caminho_entrada_saida = ler.nextLine(); 
-            
-            
-            int conv = Integer.parseInt(caminho_entrada_saida);
-            try{
-                if (conv == 909909 && InterfaceEntradaTamanho.entrada.size() == 0){
-                    throw new ArrayEmptyException("");
-                }
+        try{
+        	if (caminho.isEmpty()){
+                throw new EntradaVazioException("");
             }
-            catch(ArrayEmptyException ex){
-                System.out.println("O array n√£o pode estar vazio" + ex);
-                System.out.print( "informe os valores a serem ordenados um por vez ou 909909 para parar de adicionar: " );
-                caminho_entrada_saida = ler.nextLine();
-            }
-            finally{
-                
-            
-                conv = Integer.parseInt(caminho_entrada_saida);
-                
-                if (conv == 909909)
-                {
-                    boleano = false;
-                }
-                else
-                {
-                    InterfaceEntradaTamanho.entrada.add(conv);
-                }       
-            }
-                
-                
-            /*if (conv == 909909)
-            {
-                boleano = false;
-            }
-            else
-            {
-                InterfaceEntradaTamanho.entrada.add(conv);
-            }    */        
+		
         }
-        
-       
-        int[] arrayDesordenado = new int[ InterfaceEntradaTamanho.entrada.size()];
-        int[] arrayOrdenado = new int[InterfaceEntradaTamanho.entrada.size()];
-        for (int counter = 0; counter < (arrayDesordenado.length); counter++) 
-        {
-        arrayDesordenado[counter] = InterfaceEntradaTamanho.entrada.get(counter);
+        catch(EntradaVazioException ex1){
+        	System.out.println("O caminho n„o pode estar vazio" + ex1);
+        	System.out.println("Informe o caminho de saida do arquivo texto:\n");
+    		caminho = ler.nextLine();
         }
-       
-        
-        arrayOrdenado = ordenar.insertionSort(arrayDesordenado);
-        arrayOrdenado = ordenar.selectionSort(arrayDesordenado);
-        arrayOrdenado = ordenar.bubbleSort(arrayDesordenado);
-        arrayOrdenado = ordenar.mergeSort(arrayDesordenado);
-        arrayOrdenado = ordenar.quickSort(arrayDesordenado);
-        try {
-			arrayOrdenado = ordenar.heapSort(arrayDesordenado);
-		} catch (ArrayEmptyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.print( "o array nao pode estar vazio!: " );
-		}
-        
-
-       
-        arquivo.gravarArquivo(caminho, arrayOrdenado);
-        
+        finally
+        {
+	        boolean boleano = true;
+	        while(boleano) 
+	        {
+	        	
+	        	System.out.print( "informe os valores a serem ordenados um por vez ou 909909 para parar de adicionar: " );
+	            String caminho_entrada_saida = ler.nextLine(); 
+	            
+	            
+	            int conv = Integer.parseInt(caminho_entrada_saida);
+	            try{
+	                if (conv == 909909 && InterfaceEntradaTamanho.entrada.size() == 0){
+	                    throw new ArrayEmptyException("");
+	                }
+	            }
+	            catch(ArrayEmptyException ex){
+	                System.out.println("O array n„o pode estar vazio" + ex);
+	                System.out.print( "informe os valores a serem ordenados um por vez ou 909909 para parar de adicionar: " );
+	                caminho_entrada_saida = ler.nextLine();
+	            }
+	            finally{
+	                
+	            
+	                conv = Integer.parseInt(caminho_entrada_saida);
+	                
+	                if (conv == 909909)
+	                {
+	                    boleano = false;
+	                }
+	                else
+	                {
+	                    InterfaceEntradaTamanho.entrada.add(conv);
+	                }       
+	            }
+	                
+	                
+	            /*if (conv == 909909)
+	            {
+	                boleano = false;
+	            }
+	            else
+	            {
+	                InterfaceEntradaTamanho.entrada.add(conv);
+	            }    */        
+	        }
+	        
+	       
+	        int[] arrayDesordenado = new int[ InterfaceEntradaTamanho.entrada.size()];
+	        int[] arrayOrdenado = new int[InterfaceEntradaTamanho.entrada.size()];
+	        for (int counter = 0; counter < (arrayDesordenado.length); counter++) 
+	        {
+	        arrayDesordenado[counter] = InterfaceEntradaTamanho.entrada.get(counter);
+	        }
+	       
+	        
+	        arrayOrdenado = ordenar.insertionSort(arrayDesordenado);
+	        arrayOrdenado = ordenar.selectionSort(arrayDesordenado);
+	        arrayOrdenado = ordenar.bubbleSort(arrayDesordenado);
+	        arrayOrdenado = ordenar.mergeSort(arrayDesordenado);
+	        arrayOrdenado = ordenar.quickSort(arrayDesordenado);
+	        //try {
+				arrayOrdenado = ordenar.heapSort(arrayDesordenado);
+			//} catch (ArrayEmptyException e) {
+				//e.printStackTrace();
+			//	System.out.print( "o array nao pode estar vazio!: " );
+			//}
+	        
+	
+	       
+	        arquivo.gravarArquivo(caminho, arrayOrdenado);
+	        
+	    }
     }
 }
+  
